6c99ec531074971d6d0e2e649cb863413f3d52fa
Max Karpawich <maxkarpawich@gmail.com>
2021-04-01 18:01:32 -0400

    feat: source filter components

--- client/src/components/SourceFilter/index.js
+++ client/src/components/SourceFilter/index.js
@@ -1,8 +1,24 @@
 import './index.css';
 
+import Toggle from '../Toggle';
+
 const SourceFilter = () => {
   return (
-    <div></div>
+    <div className='Filter Sources'>
+      <h3>Source Filters</h3>
+      <Toggle name='Official only' value='official-only' />
+      <section>
+        <Toggle name='Government' value='government' />
+        <Toggle name='Media' value='media' />
+        <Toggle name='Health' value='health' />
+      </section>
+      <Toggle name='Non-official only' value='non-official-only' />
+      <Toggle name='Georgia only' value='georgia-only' />
+      <Toggle name='Health only' value='health-only' />
+      <section>
+        <Toggle name='Diabetes' value='diabetes' />
+      </section>
+    </div>
   );
 };
 
