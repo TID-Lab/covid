3a249da214fa0df89d35f876a17c436c3e650d2a
Max Karpawich <maxkarpawich@gmail.com>
2021-04-25 00:13:31 -0400

    feat: backend search query endpoint

--- server/src/api/post/index.js
+++ server/src/api/post/index.js
@@ -1,12 +1,61 @@
-const useDebug = require('debug');
+/* eslint-disable no-underscore-dangle */
+
 const routes = require('express').Router();
+const mongoose = require('mongoose');
 
-const debug = useDebug('api');
+const MongoClient = mongoose.connection.client;
+const aggieDB = MongoClient.db('aggie');
 
-// TODO remove
-routes.get('/hello', (req, res) => {
-  res.status(200).send('<h1>Hello, World!</h1>');
-  debug('Hello, World!');
+function bodyToFilter(body) {
+  const {
+    dates,
+    topics,
+    sourceFilters,
+    platforms,
+  } = body;
+
+  const filter = {};
+
+  // dates
+  const { from, to } = dates || {};
+  if (from) {
+    filter.authoredAt = { $gte: new Date(from) };
+  }
+  if (to) {
+    filter.authoredAt = {
+      ...filter.authoredAt,
+      $lte: to,
+    };
+  }
+
+  // topics
+  if (Array.isArray(topics) && topics.length > 0) {
+    filter.topics = { $all: topics };
+  }
+
+  // source filters
+  if (Array.isArray(sourceFilters) && sourceFilters.length > 0) {
+    filter['metadata.lists'] = { $all: sourceFilters };
+  }
+
+  // media
+  if (Array.isArray(platforms) && platforms.length > 0) {
+    filter._media = { $in: platforms };
+  }
+
+  return filter;
+}
+
+routes.get('/', async (req, res) => {
+  const { body } = req;
+  if (!body) {
+    res.status(400).send();
+    return;
+  }
+  const filter = bodyToFilter(body);
+  const reportsCollection = aggieDB.collection('reports');
+  const reports = await reportsCollection.find(filter).toArray();
+  res.status(200).send(reports);
 });
 
 module.exports = routes;
