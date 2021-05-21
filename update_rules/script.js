ea922f5cc275d2f1b5f2e6971727b93112c108f5
Max Karpawich <maxkarpawich@gmail.com>
2021-05-21 13:05:13 -0400

    fix: prefix Twitter env vars

--- update_rules/script.js
+++ update_rules/script.js
@@ -18,10 +18,10 @@ const MAX_RULE_COUNT = 1000;
 const MAX_RULE_LENGTH = 1024;
 
 const twitterV1 = new TwitterV1({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const twitterV2 = new TwitterV2({
