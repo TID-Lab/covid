38d9a55ea7c5fbb6061fddd14bdf3ad45cf256c3
Max Karpawich <maxkarpawich@gmail.com>
2021-05-21 15:18:39 -0400

    feat: persist lastTimestamp across runtimes

--- fetch/src/fetch.js
+++ fetch/src/fetch.js
@@ -1,6 +1,7 @@
 const { Engine, builtin } = require('downstream');
 const useDebug = require('debug');
 const { fetch: { credentials } } = require('./util/config');
+const { get, set } = require('./util/settings');
 const keywords = require('../../keywords');
 const SocialMediaPost = require('./post');
 
@@ -15,47 +16,6 @@ const {
 
 const engine = new Engine();
 
-const twitterStreamChannel = new TwitterStreamChannel({
-  credentials: credentials.twitter,
-});
-
-const twitterPageChannel = new TwitterPageChannel({
-  credentials: credentials.twitter,
-  queryParams: {
-    query: keywords.join(' OR '),
-  },
-});
-
-const facebookListChannel = new CrowdTangleFacebookChannel({
-  dashboardToken: credentials.facebook,
-  queryParams: {
-    searchTerm: keywords.join(', '),
-  },
-});
-
-const facebookPlatformChannel = new CrowdTangleFacebookChannel({
-  dashboardToken: credentials.facebook,
-  isCrossPlatform: true,
-  queryParams: {
-    searchTerm: keywords.join(', '),
-  },
-});
-
-const instaListChannel = new CrowdTangleInstagramChannel({
-  dashboardToken: credentials.instagram,
-  queryParams: {
-    searchTerm: keywords.join(', '),
-  },
-});
-
-const instaPlatformChannel = new CrowdTangleInstagramChannel({
-  dashboardToken: credentials.instagram,
-  isCrossPlatform: true,
-  queryParams: {
-    searchTerm: keywords.join(', '),
-  },
-});
-
 const saveToDatabase = async (data, next) => {
   const post = new SocialMediaPost(data);
   await post.save();
@@ -64,6 +24,69 @@ const saveToDatabase = async (data, next) => {
 
 module.exports = async () => {
   debug('Starting downstream...');
+
+  const settings = await get();
+
+  const twitterStreamChannel = new TwitterStreamChannel({
+    credentials: credentials.twitter,
+  });
+
+  const twitterPageChannel = new TwitterPageChannel({
+    credentials: credentials.twitter,
+    queryParams: {
+      query: keywords.join(' OR '),
+    },
+    lastTimestamp: settings.twitter_page_lastTimestamp,
+    onFetch: async (lastTimestamp) => {
+      await set('twitter_page_lastTimestamp', lastTimestamp);
+    },
+  });
+
+  const facebookListChannel = new CrowdTangleFacebookChannel({
+    dashboardToken: credentials.facebook,
+    queryParams: {
+      searchTerm: keywords.join(', '),
+    },
+    lastTimestamp: settings.facebook_list_lastTimestamp,
+    onFetch: async (lastTimestamp) => {
+      await set('facebook_list_lastTimestamp', lastTimestamp);
+    },
+  });
+
+  const facebookPlatformChannel = new CrowdTangleFacebookChannel({
+    dashboardToken: credentials.facebook,
+    isCrossPlatform: true,
+    queryParams: {
+      searchTerm: keywords.join(', '),
+    },
+    lastTimestamp: settings.facebook_platform_lastTimestamp,
+    onFetch: async (lastTimestamp) => {
+      await set('facebook_platform_lastTimestamp', lastTimestamp);
+    },
+  });
+
+  const instaListChannel = new CrowdTangleInstagramChannel({
+    dashboardToken: credentials.instagram,
+    queryParams: {
+      searchTerm: keywords.join(', '),
+    },
+    lastTimestamp: settings.insta_list_lastTimestamp,
+    onFetch: async (lastTimestamp) => {
+      await set('insta_list_lastTimestamp', lastTimestamp);
+    },
+  });
+
+  const instaPlatformChannel = new CrowdTangleInstagramChannel({
+    dashboardToken: credentials.instagram,
+    isCrossPlatform: true,
+    queryParams: {
+      searchTerm: keywords.join(', '),
+    },
+    lastTimestamp: settings.insta_platform_lastTimestamp,
+    onFetch: async (lastTimestamp) => {
+      await set('insta_platform_lastTimestamp', lastTimestamp);
+    },
+  });
 
   engine.register(twitterStreamChannel);
   // engine.register(twitterPageChannel);
