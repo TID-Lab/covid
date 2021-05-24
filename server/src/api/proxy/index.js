4452a1c0da06b4bfbafaaf9a48967321f68169ac
Max Karpawich <maxkarpawich@gmail.com>
2021-05-24 16:05:25 -0400

    fix: handle proxy errors better

--- server/src/api/proxy/index.js
+++ server/src/api/proxy/index.js
@@ -4,43 +4,64 @@ const { api: { proxy: { facebookToken, instagramToken } } } = require('../../uti
 
 routes.get('/twitter', async (req, res) => {
   const { url } = req.query;
-  const response = await axios({
-    method: 'GET',
-    baseURL: 'https://publish.twitter.com',
-    url: '/oembed',
-    params: { url, omit_script: '1', },
-  });
-  res.status(200).send(response.data);
+  try {
+    const response = await axios({
+      method: 'GET',
+      baseURL: 'https://publish.twitter.com',
+      url: '/oembed',
+      params: { url, omit_script: '1' },
+    });
+    res.status(200).send(response.data);
+  } catch (err) {
+    const { response: errResponse } = err;
+    if (errResponse) {
+      res.status(errResponse.status).send(errResponse.data);
+    }
+  }
 });
 
 routes.get('/facebook', async (req, res) => {
   const { url } = req.query;
-  const response = await axios({
-    method: 'GET',
-    baseURL: 'https://graph.facebook.com',
-    url: '/v10.0/oembed_post',
-    params: {
-      url,
-      access_token: facebookToken,
-      omitscript: 'true',
-    },
-  });
-  res.status(200).send(response.data);
+  try {
+    const response = await axios({
+      method: 'GET',
+      baseURL: 'https://graph.facebook.com',
+      url: '/v10.0/oembed_post',
+      params: {
+        url,
+        access_token: facebookToken,
+        omitscript: 'true',
+      },
+    });
+    res.status(200).send(response.data);
+  } catch (err) {
+    const { response: errResponse } = err;
+    if (errResponse) {
+      res.status(errResponse.status).send(errResponse.data);
+    }
+  }
 });
 
 routes.get('/instagram', async (req, res) => {
   const { url } = req.query;
-  const response = await axios({
-    method: 'GET',
-    baseURL: 'https://graph.facebook.com',
-    url: '/v10.0/instagram_oembed',
-    params: {
-      url,
-      access_token: instagramToken,
-      omitscript: 'true',
-    },
-  });
-  res.status(200).send(response.data);
+  try {
+    const response = await axios({
+      method: 'GET',
+      baseURL: 'https://graph.facebook.com',
+      url: '/v10.0/instagram_oembed',
+      params: {
+        url,
+        access_token: instagramToken,
+        omitscript: 'true',
+      },
+    });
+    res.status(200).send(response.data);
+  } catch (err) {
+    const { response: errResponse } = err;
+    if (errResponse) {
+      res.status(errResponse.status).send(errResponse.data);
+    }
+  }
 });
 
 module.exports = routes;
