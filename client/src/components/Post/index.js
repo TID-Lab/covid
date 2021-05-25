a6eb6c14991b92f4af737065860b739d5927a24c
Max Karpawich <maxkarpawich@gmail.com>
2021-05-25 16:31:41 -0400

    feat: add cover to Post component while embeds load

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,39 +1,76 @@
 import './index.css';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+const embedHTMLCache = [];
+
+function waitForEmbed(parent, callback) {
+  const isEmbedded = parent.querySelector('iframe');
+  if (isEmbedded) return setTimeout(callback, 1000);
+  setTimeout(() => {
+    waitForEmbed(parent, callback);
+  }, 10);
+}
 
 const Post = (props) => {
   const { data } = props;
-  const { url, platform } = data;
+  const { url, platform, platformID } = data;
+  const coverImagePath = `/images/${platform}.png`;
+  const elementID = `post-${platform}-${platformID}`;
+  const element = document.getElementById(elementID);
 
-  const [embeddedHTML, setEmbeddedHTML] = useState(null);
+  const [ isLoaded, setLoaded ] = useState(false);
+  const [ isRendered, setRendered ] = useState(false);
+
+  const wait = useCallback(
+    () => {
+      waitForEmbed(element, () => { setRendered(true) })
+    },
+    [ element ]
+  )
 
   useEffect(() => {
     async function fetchHTML() {
       const res = await fetch(`/api/proxy/${platform}?url=${url}`);
       const { html } = await res.json();
-      setEmbeddedHTML(html);
+      embedHTMLCache[platformID] = html;
     }
-
-    if (!embeddedHTML) {
-      fetchHTML();
+    if (!isLoaded) {
+      if (!embedHTMLCache[platformID]) {
+        fetchHTML().then(() => setLoaded(true));
+      } else {
+        setLoaded(true);
+      }
     } else {
       switch (platform) {
         case 'twitter':
-          if (window.twttr && window.twttr.widgets) window.twttr.widgets.load()
+          if (window.twttr && window.twttr.widgets) window.twttr.widgets.load(element);
           break;
         case 'instagram':
-          if (window.instgrm && window.instgrm.Embeds) window.instgrm.Embeds.process()
+          if (window.instgrm && window.instgrm.Embeds) window.instgrm.Embeds.process(element)
           break;
         case 'facebook':
-          if (window.FB && window.FB.XFBML) window.FB.XFBML.parse();
+          if (window.FB && window.FB.XFBML) window.FB.XFBML.parse(element);
           break;
         default:
       }
+      wait(platform, element);
     }
-  }, [embeddedHTML, url, platform]);
+  }, [ isLoaded, platform, platformID, url, elementID, element, wait ]);
 
-  return <div className='Post' dangerouslySetInnerHTML={{__html: embeddedHTML}}></div>
+  const embedHTML = (embedHTMLCache[platformID]) ? embedHTMLCache[platformID] : '';
+  const embedClass = 'content' + (!isRendered ? ' hidden' : '');
+
+  return (
+    <div className='Post'>
+      {(!isRendered) ? (
+        <div className='cover'>
+          <img src={coverImagePath} alt={platform} />
+        </div>
+      ): ''}
+      <div className={embedClass} id={elementID} dangerouslySetInnerHTML={{__html: embedHTML}}></div>
+    </div>
+  );
 };
 
 export default Post;
