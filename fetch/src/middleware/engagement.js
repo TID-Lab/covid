7588645b9c3391194d0fa9768973944134b322cc
amyzc <amyzc@users.noreply.github.com>
2021-06-04 10:28:17 -0400

    Added engagement score calculations

--- fetch/src/middleware/engagement.js
+++ fetch/src/middleware/engagement.js
@@ -1,17 +1,29 @@
 module.exports = async function addEngagement(post, next) {
-  // TODO add a normalized engagement metric to each post
   const engagementRaw = 0;
   const engagementNormed = 0;
 
   const { platform, raw } = post;
+
   switch (platform) {
     case 'twitter': {
+      const { quote_count, reply_count, retweet_count, favorite_count } = raw;
+      const { user: { followers_count } } = raw;
+      engagementRaw = quote_count + reply_count + retweet_count + favorite_count;
+      engagementNormed = engagementRaw / followers_count;
       break;
     }
     case 'facebook': {
+      const { statistics: { actual: { likeCount, loveCount, wowCount, hahaCount, sadCount, angryCount, thankfulCount, careCount, commentCount, shareCount } } } = raw;
+      const { subscriberCount } } = raw;
+      engagementRaw = likeCount + loveCount + wowCount + hahaCount + sadCount + angryCount + thankfulCount + careCount + commentCount + shareCount;
+      engagementNormed = engagementRaw / subscriberCount;
       break;
     }
     case 'instagram': {
+      const { statistics: { actual: { favoriteCount, commentCount } } } = raw;
+      const { subscriberCount } } = raw;
+      engagementRaw = favoriteCount + commentCount;
+      engagementNormed = engagementRaw / subscriberCount;
       break;
     }
     default:
