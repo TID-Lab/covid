6f40541f686e64e8686a8c893b89d88579ed6729
amyzc <amyzc@users.noreply.github.com>
2021-06-04 10:10:55 -0400

    Added COVID topics substrings to constants.js

--- constants.js
+++ constants.js
@@ -7,11 +7,50 @@ const COVID_KEYWORDS = [
 ];
 
 const COVID_TOPICS = {
-  'covid-diabetes': [],
-  'testing': [],
-  'vaccines': [],
-  'long-hauler': [],
-  'georgia': [],
+  'covid-diabetes': [
+    'diabet',
+    't1d',
+    't2d',
+    'insulin',
+    'blood sugar',
+    'a1c',
+    'metformin',
+    'heart disease',
+    'obes',
+    'overweight',
+  ],
+  'testing': [
+    'test',
+    'pcr',
+    'swab',
+    'negative',
+    'covid result',
+  ],
+  'vaccines': [
+    'vacc',
+    'vax',
+    'shot',
+    'astrazeneca',
+    'j&j',
+    'johnson & johnson',
+    'johnson and johnson',
+    'moderna',
+    'pfizer',
+    'mrna',
+    'one and done',
+    'oneanddone',
+  ],
+  'long-hauler': [
+    'long hauler',
+    'long-hauler',
+    'longhauler',
+    'long covid',
+    'longcovid',
+  ],
+  'georgia': [
+    'georgia',
+    'atl'
+  ],
 }
 
 module.exports = {
