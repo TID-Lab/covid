48f291fb1cdbcda68f6322f3712e871d1c30d63a
Max Karpawich <maxkarpawich@gmail.com>
2021-06-09 15:59:51 -0400

    feat: touch up look & feel of the page

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -3,7 +3,8 @@ import './index.css';
 const Logo = () => {
   return (
     <div className='Logo'>
-      <img src='logo.png' height='47pt'></img>
+      <img src='/images/projectpeach.png' alt='Project Peach'></img>
+      <h3>Project <b className='bold'>PEACH</b></h3>
     </div>
   );
 };
