e250e16b73d7e6fc0deb3118617037317eb45449
amyzc <amyzc@users.noreply.github.com>
2021-06-23 11:43:28 -0400

    Feat: make logo link to project page

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -2,10 +2,12 @@ import './index.css';
 
 const Logo = () => {
   return (
-    <div className='Logo'>
-      <img src='/images/projectpeach.png' alt='Project Peach'></img>
-      <h3>Project <b className='bold'>PEACH</b></h3>
-    </div>
+    <a href='https://www.projectpeach.org/' target='_blank' rel='noopener noreferrer'>
+      <div className='Logo'>
+        <img src='/images/projectpeach.png' alt='Project Peach'></img>
+        <h3>Project <b className='bold'>PEACH</b></h3>
+      </div>
+    </a>
   );
 };
 
