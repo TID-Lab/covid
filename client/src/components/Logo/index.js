c4a11d83ad0775ccef30e8b07c0b8bd7e0f6e2b3
amyzc <amyzc@users.noreply.github.com>
2021-07-13 13:13:52 -0400

    Changed logo to link to SM dashboard homepage

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,13 +1,19 @@
+import { Link } from 'react-router-dom';
+
 import './index.css';
 
 const Logo = () => {
   return (
-    <a href='https://www.projectpeach.org/' target='_blank' rel='noopener noreferrer'>
-      <div className='Logo'>
+    // <a href='https://www.projectpeach.org/' target='_blank' rel='noopener noreferrer'>
+    <Link to='/'>
+      <span className='Logo'>
         <img src='/images/projectpeach.png' alt='Project Peach'></img>
-        <h3>Project <b className='bold'>PEACH</b></h3>
-      </div>
-    </a>
+        <div style={{textAlign:'left', marginLeft:'0.5em'}}>  
+          <h3 style={{margin:'0px'}}>Project <b className='bold'>PEACH</b></h3>
+          Social Media Dashboard
+        </div>
+      </span>
+    </Link>
   );
 };
 
