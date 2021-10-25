5ba13760f939989af3bccf33008f9b4566fa29c6
Andrew Zhao <steveand117@gmail.com>
2021-10-25 15:50:01 -0400

    Landing Page: Added more faculty and student descriptions + pictures

--- client/src/components/AboutPage/index.js
+++ client/src/components/AboutPage/index.js
@@ -3,19 +3,25 @@ import LandingFooter from '../LandingFooter';
 const AboutPage = (props) => {
   return (<div className='AboutPage'>
       <div className='AboutSection'>
+      <h1 className="AboutPageTitle"> Additional Information </h1>
       <h1 className="AboutTitle"> Georgia Country Comparison Dashboard </h1>
       <h2 className="DataSource"> Data Sources </h2>
       <ul>
-        <li> <a href="google.com" target='_blank' rel='noopener noreferrer'> Data Source 1 </a> </li>
-        <li> <a href="google.com" target='_blank' rel='noopener noreferrer'> Data Source 2 </a> </li>
+        <li> <a href="https://dph.georgia.gov/covid-19-daily-status-report" target='_blank' rel='noopener noreferrer'> COVID outcomes data (cases, hospitalizations, deaths, testing and related data) from the Georgia Department of Public Health Daily Status Report</a> </li>
+        <li> <a href="https://www.census.gov/acs/www/data/data-tables-and-tools/data-profiles/" target='_blank' rel='noopener noreferrer'> Census demographic information (population, race, ethnicity, age distribution) from the American Community Survey 2019 5-Year Estimate by the United States Census Bureau </a> </li>
       </ul>
-      <h2 className="DataSource"> Other Information </h2>
+      {/* <h2 className="DataSource"> Other Information </h2>
       <ul>
         <li> <a href="google.com" target='_blank' rel='noopener noreferrer'> Privacy Policy </a> </li>
         <li> <a href="google.com"> Terms and Conditions </a> </li>
-      </ul>
+      </ul> */}
       <h1 className="AboutTitle"> Social Media Monitoring Dashboard </h1>
-      <h2 className="DataSource"> Other Information </h2>
+            <h2 className="DataSource"> Data Sources </h2>
+      <ul>
+        <li> <a href="https://developer.twitter.com/en/docs/twitter-api" target='_blank' rel='noopener noreferrer'> Twitter Public API </a> </li>
+        <li> <a href="https://www.crowdtangle.com/"> Crowdtangle for Instagram and Facebook posts </a> </li>
+      </ul>
+      <h2 className="DataSource"> Usage Information </h2>
       <ul>
         <li> <a href="https://peach.ipat.gatech.edu/social-media-dashboard/privacy-policy"  target='_blank' rel='noopener noreferrer'> Privacy Policy </a> </li>
         <li> <a href="https://peach.ipat.gatech.edu/social-media-dashboard/terms"  target='_blank' rel='noopener noreferrer'> Terms and Conditions </a> </li>
@@ -23,14 +29,14 @@ const AboutPage = (props) => {
       <h1 className="AboutTitle"> Georgia Capacity Modeling Dashboard </h1>
       <h2 className="DataSource"> Data Sources </h2>
       <ul>
-        <li> <a href="google.com"> Data Source 1 </a> </li>
-        <li> <a href="google.com"> Data Source 2 </a> </li>
+        <li> <a href="https://www.census.gov/acs/www/data/data-tables-and-tools/data-profiles/"> Demographic data from the Census Bureau 2019 ACS 5-year Survey </a> </li>
+        {/* <li> <a href="google.com"> SVI data: From the CDC (based on 2018 ACS 5-year Survey) </a> </li> */}
       </ul>
-      <h2 className="DataSource"> Other Information </h2>
+      {/* <h2 className="DataSource"> Other Information </h2>
       <ul>
         <li> <a href="google.com"> Privacy Policy </a> </li>
         <li> <a href="google.com"> Terms and Conditions </a> </li>
-      </ul>
+      </ul> */}
       </div>
       <LandingFooter></LandingFooter>
       </div>
