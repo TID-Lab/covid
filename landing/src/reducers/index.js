17587ddf8d2c52140b5f35d8f22584f8cd05681e
last night <fujizhang2525@gmail.com>
2022-05-12 15:23:10 -0400

    cleanup (removing reducers ver.)

--- landing/src/reducers/index.js
+++ landing/src/reducers/index.js
@@ -2,20 +2,12 @@
 
 import { combineReducers } from 'redux'
 
-import filtersReducer from './filters';
-import postsReducer from './posts';
 import popupReducer from './popup';
-import postingMenuReducer from './postingMenu';
-import postingTextReducer from './postingText';
-import postingImageReducer from './postingImage';
 
 const rootReducer = combineReducers({
-    filters: filtersReducer,
-    posts: postsReducer,
+// keeping popup just in case we need it 
     popup: popupReducer,
-    postingMenu: postingMenuReducer,
-    postingText: postingTextReducer,
-    postingImage: postingImageReducer,
+
 });
   
 export default rootReducer;
