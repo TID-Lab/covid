3109c817366a6674dff8f3495368b7da426aff1f
last night <fujizhang2525@gmail.com>
2022-05-12 15:04:48 -0400

    remove client from landing

--- landing/src/components/App/index.js
+++ landing/src/components/App/index.js
@@ -7,14 +7,7 @@ import { useSelector } from 'react-redux';
 
 import './index.css';
 
-import Header from '../Header';
-import Footer from '../Footer';
-import Dashboard from '../Dashboard';
-import Settings from '../Settings';
-import Login from '../Login';
-import PrivacyPolicy from '../PrivacyPolicy';
-import Terms from '../Terms';
-import DataDeletion from '../DataDeletion';
+import LandingHeader from '../LandingHeader';
 import LandingPage from '../LandingPage';
 import AboutPage from '../AboutPage';
 import TeamPage from '../TeamPage';
@@ -28,40 +21,20 @@ const App = () => {
     <div className='Root'>
       <div className='App'>
         <Router>
-
-          <Header />
+          
+          <LandingHeader />
           <Switch>
             <Route exact path="/">
               <LandingPage />
             </Route>
-            <Route exact path="/social-media-dashboard">
-              <Dashboard />
-            </Route>
+            
             <Route path="/about">
               <AboutPage />
             </Route>
             <Route path="/team">
               <TeamPage />
             </Route>
-            <Route path="/login">
-              <Login />
-              <Footer />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-            <Route path="/social-media-dashboard/about">
-              <PrivacyPolicy />
-            </Route>
-            <Route path="/social-media-dashboard/privacy-policy">
-              <PrivacyPolicy />
-            </Route>
-            <Route path="/social-media-dashboard/terms">
-              <Terms />
-            </Route>
-            <Route path="/dassocial-media-dashboardhboard/data-deletion">
-              <DataDeletion />
-            </Route>
+            
           </Switch>
 
         </Router>
