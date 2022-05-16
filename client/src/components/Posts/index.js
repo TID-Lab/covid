4f58db7930477cefcf3d315a7a183564624f89bc
Adam Gerig <adamgerig@gatech.edu>
2022-05-16 11:17:07 -0400

    Indication of Search Results + No Next Page Button On Last Page
    Text "No results found." displays upon search returning no results.
    
    The "Next Page" button does not appear on the last page of posts.

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -9,19 +9,25 @@ import NextPageButton from '../NextPageButton';
 
 const Posts = () => {
   const posts = useSelector(state => state.posts);
-
-  return (
-    <div className='Posts' id='Posts'>
-
-        {(page > 0) ? <PrevPageButton /> : ''}
-
-        {posts.map(post => (
-          <Post data={post} key={post.url} />
-        ))}
-
-        {(!lastPage) ? <NextPageButton /> : ''}
-    </div>
-  );
+  if (posts.length > 0) {
+    return (
+      <div className='Posts' id='Posts'>
+  
+          {(page > 0) ? <PrevPageButton /> : ''}
+  
+          {posts.map(post => (
+            <Post data={post} key={post.url} />
+          ))}
+          
+          {(!lastPage) ? <NextPageButton /> : ''}
+      </div>
+    );
+  } else {
+    return (
+      <div className='NoResults' id='NoResults'>No results found.</div>
+    )
+  }
+  
 };
 
 export default Posts;
