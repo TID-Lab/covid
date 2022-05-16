000cb966878ca2b3015bcf2af3d7f38323384594
Sandeep Prasad <s3g007@gmail.com>
2022-05-16 13:17:38 -0400

    Added cypress

---
+++ cypress/integration/fetch/topics.spec.js
@@ -0,0 +1,9 @@
+import * as fetch from '../../../fetch/src/hooks/topics.js';
+
+console.log(fetch.add);
+
+describe('Topics', () => {
+  it('Should pass', () => {
+    expect(true).to.eq(true);
+  });
+});
