8bd4f84b268067a4c015f9717925de306824172c
last night <fujizhang2525@gmail.com>
2022-06-06 22:56:22 -0400

    button typing

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,6 +1,19 @@
-// @ts-nocheck
-import PropTypes from 'prop-types';
+import React, { CSSProperties } from "react";
+
 // define button styles here
+
+export enum Style {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  OUTLINE = 'outline'
+}
+
+export enum Size {
+  SM = 'sm',
+  MD = 'md',
+  LG = 'lg',
+  XL = 'xl'
+}
 const btnStyle = {
   primary: 'bg-primary hover:bg-gray-600 text-white rounded-full', //big primary button
   secondary: 'bg-gray-100 hover:bg-gray-300 text-black rounded-md',
@@ -12,6 +25,7 @@ const btnSize = {
   sm: '',
   md: 'py1 px-4',
   lg: 'py-2 px-6 ',
+  xl: ''
 };
 
 const Button = ({
@@ -21,27 +35,31 @@ const Button = ({
   onClick,
   className,
   style,
-  variant = 'primary',
-  size = 'lg',
-}) => {
+  variant = Style.PRIMARY,
+  size = Size.LG,
+}:ButtonProps) => {
   return (
     <button
       type="button"
       id={id}
       className={`grid gap-4 auto-cols-auto ${btnStyle[variant]} ${btnSize[size]} ${className}`}
       style={style}
-      onClick={onClick}
-      onKeyDown={onClick}
+      onClick={()=> onClick()}
+      //onKeyDown={()=>onClick()}
       disabled={disabled}
     >
       {children}
     </button>
   );
 };
-// replace with typescript way in the future
-Button.propTypes = {
-  variant: PropTypes.oneOf(['primary', 'secondary', 'transparent', 'outline']),
-  size: PropTypes.oneOf(['sm', 'md', 'lg']),
-};
-
+interface ButtonProps {
+  children?: React.ReactNode
+  disabled?: boolean
+  id?: string
+  onClick?:any
+  className?: string
+  style?: CSSProperties
+  variant?: Style
+  size?: Size
+}
 export default Button;
