f7b8afea365a71cb247930ca5016b7488184af40
last night <fujizhang2525@gmail.com>
2022-06-17 22:05:08 -0400

    make legalities more readable

--- client/src/pages/DataDeletion/index.tsx
+++ client/src/pages/DataDeletion/index.tsx
@@ -1,30 +1,27 @@
-import useTracker from "hooks/useTracker";
-import { useEffect } from "react";
+import useTracker from 'hooks/useTracker';
+import { useEffect } from 'react';
 
 const DataDeletion = () => {
-  const {trackPageView} = useTracker();
- useEffect(() => {
-    trackPageView()
-  }, [])
-  
+  const { trackPageView } = useTracker();
+  useEffect(() => {
+    trackPageView();
+  }, []);
+
   return (
-    <div className="Policy">
-      <h1>Data Deletion Instructions</h1>
+    <div className="Policy overflow-auto">
+      <div className="container mx-auto my-9 space-y-5">
+        <h1>Data Deletion Instructions</h1>
 
-      <br />
-
-      <p>
-        If you, either as a user of the Project PEACH Social Media Dashboard or
-        as a user of a social media platform with data collected by our
-        dasbhoard, would like to have your data deleted, please contact us at{' '}
-        <a href="mailto:mikeb@gatech.edu">mikeb@gatech.edu</a>. If you are a
-        social media platform user with data collectd by our dashboard, please
-        provide us your username for the relevant social media platform(s) so
-        that we can identify the data which needs to be deleted.
-      </p>
-
-      <br />
-      <br />
+        <p>
+          If you, either as a user of the Project PEACH Social Media Dashboard
+          or as a user of a social media platform with data collected by our
+          dasbhoard, would like to have your data deleted, please contact us at{' '}
+          <a href="mailto:mikeb@gatech.edu">mikeb@gatech.edu</a>. If you are a
+          social media platform user with data collectd by our dashboard, please
+          provide us your username for the relevant social media platform(s) so
+          that we can identify the data which needs to be deleted.
+        </p>
+      </div>
     </div>
   );
 };
