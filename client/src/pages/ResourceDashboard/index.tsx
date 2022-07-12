e7cb9425ed78101d056f56efec61270545e56e0c
simon <fujizhang2525@gmail.com>
2022-07-12 17:02:17 -0400

    Update client/src/pages/ResourceDashboard/index.tsx
    Co-authored-by: Adam Gerig <67169800+topperblue@users.noreply.github.com>

--- client/src/pages/ResourceDashboard/index.tsx
+++ client/src/pages/ResourceDashboard/index.tsx
@@ -13,7 +13,7 @@ import useTracker from 'hooks/useTracker';
 // This is the resources dashboard and it assembles the resource posts and filters associated with it.
 
 const ResourceDashboard = () => {
-  const {trackPageView} = useTracker();
+  const { trackPageView } = useTracker();
 
   useAuth();
   
