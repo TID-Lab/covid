23afb13ac2caee8d6d42e055d7e77f77b6ec9df1
Andrew Zhao <steveand117@gmail.com>
2022-07-13 06:06:06 -0400

    Added basic backend unit tests

---
+++ cypress/integration/frontend/createPost.spec.js
@@ -0,0 +1,6 @@
+// TODO template, refer to https://docs.cypress.io/guides/component-testing/quickstart-react#Configuring-Component-Testing 
+// or possibly https://docs.cypress.io/guides/end-to-end-testing/writing-your-first-end-to-end-test
+
+it('visit the site', () => {
+  cy.visit("/")
+});
