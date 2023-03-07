2a842b3be33978d601d143bd6cf83161b8f24df2
Andrew Zhao <steveand117@gmail.com>
2023-03-07 02:52:30 -0500

    Quick removal of eslint for prod (can figure out later)

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   'extends': [
     'react-app',
     'react-app/jest',
-    '../eslintrc-react-base.js',
-    '../eslintrc-react-plugins.js'
+    // '../eslintrc-react-base.js',
+    // '../eslintrc-react-plugins.js'
 
   ],
   rules: {
